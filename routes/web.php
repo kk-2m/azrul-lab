70480fc969c6aca2423804d54a7426c04489de47
kk-2m <kishi.koki6@gmail.com>
2024-03-13 16:29:45 +0800

    controller settings

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeacherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+// Route::get('/teachers', [TeacherController::class, 'index']);
+Route::get('/teachers', function() {
+    return view('profile.teacher');
 });
